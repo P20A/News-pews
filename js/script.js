e60cadfe62209b00ab0fed6dfe058a8272b1d5b5
parsa <parsa20ataei@gmail.com>
2025-09-06 19:25:54 +0330

    back to top button added

--- js/script.js
+++ js/script.js
@@ -5,3 +5,21 @@ const body = document.querySelector('body')
 darkMode.addEventListener('click' ,function(){
     body.classList.toggle('darkmode')
 })
+
+// back to top btn
+let mybutton = document.getElementById("backToTop");
+
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+function topFunction() {
+  document.body.scrollTop = 0; 
+  document.documentElement.scrollTop = 0; 
+}
